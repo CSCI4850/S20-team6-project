ba78f9edbe9f06cad28e756fa7078820ffb60a89
nibraaska <nnibraas@gmail.com>
2020-04-05 17:30:47 -0400

    Preprocess fix

--- preprocess_images.py
+++ preprocess_images.py
@@ -13,14 +13,16 @@
 # ---
 
 import random
+import sys
 import numpy as np
 from sten import Sten
 import os.path
 from tqdm.auto import tqdm, trange
 
-NUM_FILES = 100
+NUM_FILES = int(sys.argv[1])
 
 st = Sten(3)
+done = []
 
 # +
 path = os.getcwd() + '/data/set1'
@@ -30,8 +32,14 @@ path = os.getcwd() + '/data/set2'
 num_files_set2 = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
 # -
 
-for i in trange(NUM_FILES, desc='File', leave=True):
+i = 0
+for _ in trange(NUM_FILES, desc='File', leave=True):
     set1File = random.randint(1,num_files_set1)
     set2File = random.randint(1,num_files_set2)
-    encImg = st.encode("./data/set1/{}.jpg".format(set1File), "./data/set2/{}.jpg".format(set2File), "./encodedArray/{}.npy".format(i))
-    decImg = st.decode("./decodedArray/{}.jpg".format(i), "./encodedArray/{}.npy".format(i))
+    name = str(set1File) + '.' + str(set2File)
+    if name not in done:
+        encImg = st.encode("./data/set1/{}.jpg".format(set1File), "./data/set2/{}.jpg".format(set2File), "./encodedArray/{}.npy".format(i))
+        decImg = st.decode("./encodedArray/{}.npy".format(i), "./decodedArray/{}.npy".format(i))
+        done.append(name)
+        i += 1
+print(len(done))
