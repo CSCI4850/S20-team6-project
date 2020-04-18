f5328542a7488e25d712021b39aad12c58d58bf5
Ramin-Daneshi <ramindaneshi007@gmail.com>
2020-04-18 00:49:24 +0000

    added plots

--- preprocess_images.py
+++ preprocess_images.py
@@ -13,25 +13,43 @@
 # ---
 
 import random
+import sys
 import numpy as np
 from sten import Sten
 import os.path
 from tqdm.auto import tqdm, trange
+import pathlib
 
-NUM_FILES = 100
+#NUM_FILES = int(sys.argv[1])
 
-st = Sten(3)
+#st = Sten(3)
+#done = []
 
 # +
-path = os.getcwd() + '/data/set1'
-num_files_set1 = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
+#path = os.getcwd() + '/data/set1'
+#num_files_set1 = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
 
-path = os.getcwd() + '/data/set2'
-num_files_set2 = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
+#path = os.getcwd() + '/data/set2'
+#num_files_set2 = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
 # -
 
-for i in trange(NUM_FILES, desc='File', leave=True):
-    set1File = random.randint(1,num_files_set1)
-    set2File = random.randint(1,num_files_set2)
-    encImg = st.encode("./data/set1/{}.jpg".format(set1File), "./data/set2/{}.jpg".format(set2File), "./encodedArray/{}.npy".format(i))
-    decImg = st.decode("./decodedArray/{}.jpg".format(i), "./encodedArray/{}.npy".format(i))
+#i = 0
+#for _ in trange(NUM_FILES, desc='File', leave=True):
+#    set1File = random.randint(1,10)
+#    set2File = random.randint(1,5)
+#    name = str(set1File) + '.' + str(set2File)
+#    if name not in done:
+#        encImg = st.encode("./data/set1/{}.jpg".format(set1File), "./data/set2/{}.jpg".format(set2File), "./encodedArray_5/{}.npy".format(i))
+#        decImg = st.decode("./encodedArray_5/{}.npy".format(i), "./decodedArray_5/{}.npy".format(i))
+#        done.append(name)
+#        i += 1
+        
+for x in trange(7, 2, -1):
+    st = Sten(x)
+    pathlib.Path("./encodedArray/bit_{0}".format(x)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path("./decodedArray/bit_{0}".format(x)).mkdir(parents=True, exist_ok=True)
+    for set1File in trange(1, int(sys.argv[1])+1):
+        for set2File in trange(1, int(sys.argv[2])+1):
+            name = str(set1File) + '_' + str(set2File)
+            encImg = st.encode("./data/set1/{}.jpg".format(set1File), "./data/set2/{}.jpg".format(set2File), "./encodedArray/bit_{0}/{1}.npy".format(x, name))
+            decImg = st.decode("./encodedArray/bit_{0}/{1}.npy".format(x, name), "./decodedArray/bit_{0}/{1}.npy".format(x, name))
